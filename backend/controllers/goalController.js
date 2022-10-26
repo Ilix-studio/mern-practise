42e2baa1bbcd0ca9133fc19bb0e9ab387bb75cef
Ilix-studio <ilishjyoti17@gmail.com>
2022-10-27 03:07:27 +0530

    5th commit

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,38 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require("express-async-handler");
+const Goal = require("./../models/goalModel");
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get Goals'})
-})
+  res.status(200).json({ message: "Get Goals" });
+});
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 
 const setGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Set Goals'})
-})
+  res.status(200).json({ message: "Set Goals" });
+});
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Update Goals ${req.params.id}'})
-})
+  res.status(200).json({ message: "Update Goals ${req.params.id}" });
+});
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Delete Goals ${req.params.id}'})
-})
+  res.status(200).json({ message: "Delete Goals ${req.params.id}" });
+});
 
 module.exports = {
-    getGoals,
-    setGoal,
-    updateGoal,
-    deleteGoal,
-  }
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
